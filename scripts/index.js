7166b08e2e6956ea9baf1ac0c660d2fe0cbecec0
raycodes27 <rcastillo_dev@outlook.com>
2025-08-12 00:54:24 -0400

    add form submit functionality

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,11 @@ const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 
+
+
 editProfileBtn.addEventListener("click", function(){
     editProfileModal.classList.add("modal_is-opened");
+    handleProfileFormSubmit();
 });
 
 editProfileCloseBtn.addEventListener("click", function(){
@@ -23,3 +26,25 @@ newPostBtn.addEventListener("click", function () {
 newPostModalCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+const profileFormElement = document.querySelector(".profile__column");
+
+const profileNameElement = profileFormElement.querySelector(".profile__name");
+const profileDescriptionElement = profileFormElement.querySelector(
+  ".profile__description"
+);
+
+const nameInput = editProfileModal.querySelector("#profile-name-input");
+const jobInput = editProfileModal.querySelector("#profile-description-input");
+
+const modalSubmitBtn = editProfileModal.querySelector(".modal__submit-btn");
+modalSubmitBtn.addEventListener("click", handleProfileFormSubmit);
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  nameInput.value = profileNameElement.textContent;
+  jobInput.value = profileDescriptionElement.textContent;
+
+  editProfileModal.classList.remove("modal_is-opened");
+}
